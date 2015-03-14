e794c3bfc61d368a967cc5cae8686806984c614b
Juan A. Moyano <wincus@gmail.com>
2015-03-14 15:34:58 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -1,2 +1,23 @@
-GIT:
-  - ¿y que pasa cuando renombramos un archivo?
+#!/bin/bash
+
+#ejemplo de como renombrar un archivo
+#no modifica el objeto de tipo de blob
+#ya que no cambia su contenido, sino 
+#el objeto de tipo tree que contiene
+#la estructura
+
+cd /tmp/git-testing
+
+git mv README LEEME
+git status
+
+git commit -m "moved README to LEEME"
+
+echo "Nuevo Tree:"
+git cat-file -p c6c89
+echo "Viejo Tree:"
+git cat-file -p bc5f
+
+echo "hay dos objetos de tipo tree"
+
+exit 0
