d6b3a012adbe31e83807fb6605c67d946b142bdd
Juan A. Moyano <wincus@gmail.com>
2015-03-14 15:57:12 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -2,3 +2,4 @@ GIT:
   - Enfoque de bajo nivel
     - GIT como key/value store
       - le das un objeto y te devuelve un key unívoco de ese objeto
+      - le das el key del objeto y te devuelve el objeto original
