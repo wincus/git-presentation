a68fb94c87d0c7cf24d744bf6dbfaa47b6c55bce
Juan A. Moyano <wincus@gmail.com>
2015-03-14 16:33:53 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -1,16 +1,2 @@
-#!/bin/bash
-
-#ejemplo de como Git almacena los objetos de 
-#tipo tree
-
-cd /tmp/git-testing
-
-echo "contenido del archivo Readme" > README
-git add README
-git commit -m "Added Readme"
-
-echo "Nuevo tree bc5f:"
-git cat-file -t bc5f
-git cat-file -p bc5f
-
-exit 0
+GIT:
+  - ¿y que pasa cuando renombramos un archivo?
