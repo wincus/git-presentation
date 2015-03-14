4c9195032a3422d16ff01e58e809d84eb434ea34
Juan A. Moyano <wincus@gmail.com>
2015-03-14 19:15:28 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -1,6 +1,2 @@
 GIT:
-  - Repasando
-    - Tipos de Objeto
-      - blobs: es el contenido de un archivo
-      - trees: es un contenedor de blobs y trees
-      - commits: representan un estado con autor
+  - Referencias
