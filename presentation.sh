4e2a0e1d3480160d268769fb7ab011a33c2c2841
Juan A. Moyano <wincus@gmail.com>
2015-03-14 16:04:56 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -4,3 +4,5 @@ GIT:
       - le das un objeto y te devuelve un key unívoco de ese objeto
       - le das el key del objeto y te devuelve el objeto original
       - a los objetos los llamamos blobs
+      - y el key para recuperar el objeto original no es mas que el
+        hash sha1 del objeto
