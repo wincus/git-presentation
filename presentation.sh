17d39467971a0f5310906b6657259c5f480c829e
Juan A. Moyano <wincus@gmail.com>
2015-03-14 19:25:33 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -1,15 +1,2 @@
-#!/bin/bash
-
-# referencias en git
-
-cd /tmp/git-testing
-find .git/refs
-
-echo "hay solo una referencia creada llamada master"
-cat .git/refs/heads/master
-
-echo "vemos que el id al que apunta master es el último commit"
-git cat-file -p $(cat .git/refs/heads/master)
-
-
-exit 0
+GIT:
+  - Referencias
