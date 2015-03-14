71305c724aef884dac0ee12ae155b9e1a9b63b95
Juan A. Moyano <wincus@gmail.com>
2015-03-14 19:37:01 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -1,16 +1,2 @@
-#!/bin/bash
-
-#los branches son solo referencias
-
-cd /tmp/git-testing
-
-echo "solo un branch llamado master:"
-echo "que apunta a mi ultimo commit"
-git branch -v
-echo "Creamos un branch nuevo"
-git branch MiNuevoBranch
-echo "Master y MiNuevoBranch apuntan al mismo objeto commit"
-cat .git/refs/heads/MiNuevoBranch
-cat .git/refs/heads/master
-
-exit 0
+GIT:
+  - Referencias
