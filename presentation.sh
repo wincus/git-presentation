e7bec679ddbd5ec9195cb7c58ea48417975a1fb5
Juan A. Moyano <wincus@gmail.com>
2015-03-14 16:04:31 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -3,3 +3,4 @@ GIT:
     - GIT como key/value store
       - le das un objeto y te devuelve un key unívoco de ese objeto
       - le das el key del objeto y te devuelve el objeto original
+      - a los objetos los llamamos blobs
