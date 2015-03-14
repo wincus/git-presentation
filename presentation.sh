c10cc9967781b3fc18453268bc9877df7b1d00d9
Juan A. Moyano <wincus@gmail.com>
2015-03-14 19:13:19 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -1,17 +1,2 @@
-#!/bin/bash
-
-#anatomía de un commit:
-#  autor
-#  fecha
-#  comentario
-#  tree
-#  parent
-
-cd /tmp/git-testing
-
-echo "Commit 30d0:"
-git cat-file -p 30d0
-echo "Tree asociado al commit 30d0:"
-git cat-file -p fbee
-
-exit 0
+GIT:
+  - Repasando
