13a31c8766b49ae8e6072c87d0dda850f8c5579f
Juan A. Moyano <wincus@gmail.com>
2015-03-14 16:40:45 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -1,19 +1,2 @@
-#!/bin/bash
-
-#ejemplo de como un objeto de tipo
-#tree puede contener blobs y trees
-
-cd /tmp/git-testing
-
-mkdir subdir
-echo "subdir contents" >> subdir/file
-
-git add subdir
-git commit -m "Added subdir"
-
-echo "nuevo objeto tipo tree, e6b8:"
-git cat-file -p e6b8
-echo "nuevo objeto tipo tree, 7a61:"
-git cat-file -p 7a61
-
-exit 0
+GIT:
+  - Repasando
