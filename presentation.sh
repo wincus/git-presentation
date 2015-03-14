fda2e5a4b9a836d1d472f3a54c58269c691b7e99
Juan A. Moyano <wincus@gmail.com>
2015-03-14 16:33:09 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -1,19 +1,3 @@
-#!/bin/bash
-
-# ejemplo de GIT como un store de key/value
-
-mkdir -p /tmp/git-testing
-cd /tmp/git-testing
-
-echo "Hola Mundo!" >> saludo
-
-git add saludo
-git commit -m "Nuevo Saludo"
-
-git cat-file -t d39c
-git cat-file -s d39c
-git cat-file -p d39c
-
-echo "git contiene un nuevo blob, el d39c"
-
-exit 0
+GIT:
+  - Enfoque de bajo nivel
+    - GIT como key/value store
