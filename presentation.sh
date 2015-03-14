a82f64835f4bfb94dd08bd6f4d447039d4f1504a
Juan A. Moyano <wincus@gmail.com>
2015-03-14 16:29:04 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -1,10 +1,4 @@
 GIT:
   - Enfoque de bajo nivel
     - GIT como key/value store
-      - hasta ahora solo guardamos archivos
-      - y los recuperamos con su key
-      - pero los blobs no tienen ni estructura
-      - ni metadata
-      - Para que Git pueda gestionar la estructura y la metadata
-      - contempla un nuevo tipo de objeto que puede almacenar
-      - el objeto de tipo Tree
+      - el objeto de tipo Tree puede contener blobs o mas subtrees
