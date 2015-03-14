17b2538224b3c054aacd8799dae10cdf940a1f80
Juan A. Moyano <wincus@gmail.com>
2015-03-14 16:29:18 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -2,3 +2,5 @@ GIT:
   - Enfoque de bajo nivel
     - GIT como key/value store
       - el objeto de tipo Tree puede contener blobs o mas subtrees
+      - puede verse como un snapshot de un conjunto de archivos
+        y directorios
