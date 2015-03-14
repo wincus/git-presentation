310e2cdd1dd3cb20734d371683b55ce1db029ec8
Juan A. Moyano <wincus@gmail.com>
2015-03-14 16:35:59 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -2,15 +2,13 @@
 
 # ejemplo de GIT como un store de key/value
 
-rm -rf /tmp/git-testing
 mkdir -p /tmp/git-testing
 cd /tmp/git-testing
-git init
-echo "Hello World!" > saludo
-git hash-object saludo
-git add saludo
-git commit -m "Nuevo Saludo"
 
-find .git/objects
+echo "Nuestro archivo es el blob 980a"
+
+git cat-file -t 980a
+git cat-file -s 980a
+git cat-file -p 980a
 
 exit 0
