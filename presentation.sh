ee8eeb8bc2f2d3b172a6a4207cf3fe6c1a284cfd
Juan A. Moyano <wincus@gmail.com>
2015-03-15 11:03:04 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -3,3 +3,4 @@ GIT:
     - Son referencias, como los branches
     - La diferencia es que GIT los trata como read only
     - O sea podés hacer checkout
+    - pero GIT nunca apuntará HEAD a una rama remota
