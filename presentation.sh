f1e2f21b05fa15fb887b8946b5b6cfc3a003870f
Juan A. Moyano <wincus@gmail.com>
2015-03-15 00:42:42 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -1,19 +1,10 @@
-GIT:
-  - Commits
-    - Nombres que puede tomar un commit
-      - Branchname
-      - HEAD
-      - c82a22c39cbc32...
-      - c82a22c
-      - name^
-      - name^^
-      - name^2
-      - name~10
-      - name:path
-      - name^{tree}
-      - name1..name2
-      - name1...name2
-      - master..
-      - ..master
-      - ejemplos
+#!/bin/bash
 
+cd /tmp/git-testing
+
+git cat-file -p HEAD
+git cat-file -p HEAD^
+git cat-file -p HEAD^^
+git cat-file -p HEAD^{tree}
+
+exit 0
