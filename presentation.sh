0ae0c53d911e598909ba3432805dd52c229b4042
Juan A. Moyano <wincus@gmail.com>
2015-03-14 23:56:47 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-
-cd /tmp/git-testing
-
-echo "EOF" >> saludo
-
-git add saludo
-git commit -m "added eof to saludo"
-
-echo "Master y MiNuevoBranch ya no apuntan al mismo commit"
-cat .git/refs/heads/MiNuevoBranch
-cat .git/refs/heads/master
-
-exit 0
+GIT:
+  - Referencias:
+    - Y como sabe GIT en que Branch realizar el commit?
