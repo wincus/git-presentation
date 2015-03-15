4169d94140f371cd655526f9005b96aed2629827
Juan A. Moyano <wincus@gmail.com>
2015-03-15 00:54:10 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -1,17 +1,2 @@
 GIT:
-  - Merge
-    - Considerando este escenario:
-
-         . <- A <- B <- C <- D
-              ^ 
-              |
-              \_ W <- X <- Y <- Z
-
-    - Para unir ambas ramas:
-      - git checkout Z
-      - git merge D
-
-      . <- A <- B <- C <- D <---
-           ^                     \
-           |                       Z'(Z+D)
-           \_ W <- X <- Y <- Z <-/
+  - Rebase
