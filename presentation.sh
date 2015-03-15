cbf5a4673612dc2c4065d93c489c9af12c0994b2
Juan A. Moyano <wincus@gmail.com>
2015-03-15 00:33:42 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
-
-cd /tmp/git-testing
-
-echo "HEAD es una referencia (como un link simbolico)"
-echo "que apunta a reds/heads/master"
-
-cat .git/HEAD
-
-echo "al cambiar de branch, se cambia al HEAD"
-git checkout MiNuevoBranch
-
-echo "ahora el Head apunta a mi branch"
-cat .git/HEAD
-
-exit 0
+GIT:
+  - Repasando
+   
