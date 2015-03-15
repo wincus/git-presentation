81cd3901232204c20e52f145a2e44095964a43ec
Juan A. Moyano <wincus@gmail.com>
2015-03-14 23:54:14 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -1,3 +1,14 @@
-GIT:
-  - Referencias
-    - Y que pasa cuando hacemos un commit?
+#!/bin/bash
+
+cd /tmp/git-testing
+
+echo "EOF" >> saludo
+
+git add saludo
+git commit -m "added eof to saludo"
+
+echo "Master y MiNuevoBranch ya no apuntan al mismo commit"
+cat .git/refs/heads/MiNuevoBranch
+cat .git/refs/heads/master
+
+exit 0
