070479c37272799285d955ac6fd28cea1d6d3119
Juan A. Moyano <wincus@gmail.com>
2015-03-14 23:58:31 -0300

    new slide

--- presentation.sh
+++ presentation.sh
@@ -1,5 +1,16 @@
-GIT:
-  - Referencias:
-    - Y como sabe GIT en que Branch realizar el commit?
-    - HEAD
-    - Ejemplo
+#!/bin/bash
+
+cd /tmp/git-testing
+
+echo "HEAD es una referencia (como un link simbolico)"
+echo "que apunta a reds/heads/master"
+
+cat .git/HEAD
+
+echo "al cambiar de branch, se cambia al HEAD"
+git checkout MiNuevoBranch
+
+echo "ahora el Head apunta a mi branch"
+cat .git/HEAD
+
+exit 0
